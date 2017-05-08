f9a3c4a8136b3a63e5a21f8f4afbe9045011c08d
Ndr <sebas981@gmail.com>
2017-05-08 12:06:42 +0200

    Application des nouveaux assets
    Y'a du mieux

--- Menu/mainMenu.js
+++ Menu/mainMenu.js
@@ -17,14 +17,14 @@ var x = 540, y = 700; //Position raquette
 var move = true; //Activation de la raquette
 
 var raquetteImg = new Image();
-raquetteImg.src = "Raquette.png"; //Asset graphique barre
+raquetteImg.src = "gfx/Raquette.png"; //Asset graphique barre
 raquetteImg.width = 200;
 raquetteImg.height = 50; //Dimensions asset barre
 
-var raquetteMoveSfx = new Audio("Select.wav");
-var raquetteLockedSfx = new Audio("OutSelect.wav");
-var raquetteLaunchSfx = new Audio("Launch.wav");
-var audioBG = new Audio("journey.mp3");
+var raquetteMoveSfx = new Audio("sfx/Select.wav");
+var raquetteLockedSfx = new Audio("sfx/OutSelect.wav");
+var raquetteLaunchSfx = new Audio("sfx/Launch.wav");
+var audioBG = new Audio("sfx/journey.mp3");
 audioBG.volume = 0.3;
 audioBG.loop = true;
 audioBG.play();
@@ -36,27 +36,30 @@ var flag = false; //Activation de la balle
 var pas = 1; //Vitesse animation
 
 var balleImg = new Image();
-balleImg.src = "balle.png";
+balleImg.src = "gfx/balle.png";
 balleImg.width = 50;
 balleImg.height = 50;
 var rayon = 25; //Rayon balle
 
+var controlsHelp = new Image();
+controlsHelp.src = "gfx/ctrl.png";
+
 //Variables briques
 
 var spaceImg = new Image();
-spaceImg.src = "briqueSpace.png";
+spaceImg.src = "gfx/briqueSpace.png";
 
 var nordicImg = new Image();
-nordicImg.src = "briqueNordic.png";
+nordicImg.src = "gfx/briqueNordic.png";
 
 var troisImg = new Image();
-troisImg.src = "briquePH1.png";
+troisImg.src = "gfx/briqueMars.png";
 
 var quatreImg = new Image();
-quatreImg.src = "briquePH2.png";
+quatreImg.src = "gfx/briqueOrbit.png";
 
 var creditsImg = new Image();
-creditsImg.src = "briqueCredits.png";
+creditsImg.src = "gfx/briqueCredits.png";
 
 
 
@@ -116,6 +119,10 @@ document.addEventListener("keydown", function (event) {     // commande barre
             raquetteLaunchSfx.play();
             animation();
             break;
+        case 80:
+            alert("Lancement niveau proto");
+            location.replace("../Proto Anim/levelProto.html");
+            break;
         }
     }
 });
@@ -127,11 +134,12 @@ animation = function () {
     scene.clearRect(0, 0, 1280, 800);
     scene.beginPath();
     scene.drawImage(balleImg, posx, posy, 50, 50);
-    scene.drawImage(spaceImg, 45, 330, 190, 60);
-    scene.drawImage(nordicImg, 295, 330, 190, 60);
-    scene.drawImage(troisImg, 545, 330, 190, 60);
-    scene.drawImage(quatreImg, 795, 330, 190, 60);
+    scene.drawImage(nordicImg, 45, 330, 190, 60);
+    scene.drawImage(quatreImg, 295, 330, 190, 60);
+    scene.drawImage(spaceImg, 545, 330, 190, 60);
+    scene.drawImage(troisImg, 795, 330, 190, 60);
     scene.drawImage(creditsImg, 1045, 330, 190, 60);
+    scene.drawImage(controlsHelp, 1080, 712, 200, 88);
     scene.drawImage(raquetteImg, x, y, raquetteImg.width, raquetteImg.height);
     scene.closePath();
     scene.fill();
@@ -153,22 +161,22 @@ select = function () {
         if (posx === 115) {
             //1er choix
             flag = false;
-            alert("N'est pas encore prêt");
+            alert("Niveau Terre : Pas prêt");
             location.reload();
         } else if (posx === 365) {
             //2e choix
             flag = false;
-            alert("Encore moins prêt...");
+            alert("Niveau Orbite : Pas prêt");
             location.reload();
         } else if (posx === 615) {
             //3e choix
             flag = false;
-            alert("Lancement niveau proto");
-            location.replace("../Proto Anim/levelProto.html");
+            alert("Niveau Espaaaaaace ! : Pas prêt");
+            location.reload();
         } else if (posx === 865) {
             //4e choix
             flag = false;
-            alert("Meuuuurge !");
+            alert("Niveau Mars : Pas prêt");
             location.reload();
         } else if (posx === 1115) {
             //5e choix
